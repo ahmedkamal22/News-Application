208d1f15d7ea4d04ba2d7430cd7693a9d8f3925c
ahmedkamal22 <ak0850000@gmail.com>
2021-05-10 05:51:18 +0300

    Created Api configs Edited Home design

---
+++ app/src/main/java/com/kamal/newsapplication/Constants.kt
@@ -0,0 +1,5 @@
+package com.kamal.newsapplication
+
+object Constants {
+    val ApiKey:String = "1e1ae2ded234403aafe60f283df46940"
+}
--- app/src/main/java/com/kamal/newsapplication/HomeActivity.kt
+++ app/src/main/java/com/kamal/newsapplication/HomeActivity.kt
@@ -1,9 +1,10 @@
 package com.kamal.newsapplication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class HomeActivity : AppCompatActivity() {
+import android.os.Bundle
+import com.example.islami.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
